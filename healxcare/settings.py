f4225af4e4430b3a9d95ad669c435475ddd6a8ec
Om Kolhe <110466889+kolheom1311@users.noreply.github.com>
2025-08-27 02:36:58 +0530

    Update settings.py

--- healxcare/settings.py
+++ healxcare/settings.py
@@ -97,7 +97,7 @@ SOCIALACCOUNT_ADAPTER = "hospital.adapters.MySocialAccountAdapter"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -179,6 +179,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for collected files
+
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
